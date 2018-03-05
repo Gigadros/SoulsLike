2d3e05ca1ad87cb72f1260e2cb8085ad2bb47aaa
Jordan Knight <jordan_r_knight@hotmail.com>
2018-03-05 05:31:43 -0500

    Basic Setup
    Started a new Unity project and modified the readme file. Added models and animations. Created an animation controller for the player. Created a helper script to test the animator.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SoulsLike
 Unity 3rd person action rpg game inspired by Dark Souls
+
+## About 
+
+This project was created in Unity 2017 with resources and guidance from www.sharpaccent.com
---
+++ Soulslike/Assets/Scripts/Utilities/Helper.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA {
+	public class Helper : MonoBehaviour {
+
+		[Range(-1,1)]
+		public float vertical;
+		[Range(-1,1)]
+		public float horizontal;
+
+		public bool playAnim;
+		public string[] oh_attacks;
+		public string[] th_attacks;
+
+		public bool twoHanded;
+		public bool enableRM;
+		public bool useItem;
+		public bool interacting;
+		public bool lockon;
+
+		Animator anim;
+
+		void Start () {
+			anim = GetComponent<Animator> ();
+		}
+
+		void Update () {
+
+			enableRM = !anim.GetBool ("canMove");
+			anim.applyRootMotion = enableRM;
+
+			interacting = anim.GetBool ("interacting");
+
+			if (!lockon) {
+				horizontal = 0;
+				vertical = Mathf.Clamp01 (vertical);
+			}
+
+			anim.SetBool ("lockon", lockon);
+
+			if (enableRM)
+				return;
+
+			if (useItem) {
+				anim.Play ("use_item");
+				useItem = false;
+			}
+
+			if (interacting) {
+				playAnim = false;
+				vertical = Mathf.Clamp (vertical, 0, 0.5f);
+			}
+
+			anim.SetBool ("two_handed", twoHanded);
+
+			if (playAnim) {
+				string targetAnim;
+
+				if (!twoHanded) {
+					int r = Random.Range (0, oh_attacks.Length);
+					targetAnim = oh_attacks [r];
+				} else {
+					int r = Random.Range (0, th_attacks.Length);
+					targetAnim = th_attacks [r];
+				}
+				if (vertical > 0.5f) {
+					targetAnim = "oh_attack_3";
+				}
+
+				vertical = 0;
+				anim.CrossFade (targetAnim, 0.2f);
+				//anim.SetBool ("canMove", false);
+				//enableRM = true;
+				playAnim = false;
+			}
+
+			anim.SetFloat ("vertical", vertical);
+			anim.SetFloat ("horizontal", horizontal);
+		}
+	}
+}
